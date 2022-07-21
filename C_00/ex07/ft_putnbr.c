0f965c8d04d6660d251680bbbd2ecfa5dd821c1e
Marcella Carvalho Agostinho <marcarva@c3r5p3.42sp.org.br>
2022-07-21 12:22:56 -0300

    Adicionar o ex07

---
+++ C_00/ex07/ft_putnbr.c
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marcarva <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/20 15:32:39 by marcarva          #+#    #+#             */
+/*   Updated: 2022/07/20 15:32:42 by marcarva         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include<unistd.h>
+
+void	ft_putnbr(int nb)
+{
+	int		res;
+	char	c;
+	// Transformando nb para positivo e tratando o caso limite negativo
+	if (nb == -2147483648)
+	{
+		write(1, "-", 1);
+		write(1, "2", 1);
+		nb = 147483648;
+	}
+	if (nb > -2147483648 && nb < 0)
+	{
+		nb = nb * (-1);
+		write(1, "-", 1);
+	}
+
+	// Sendo nb positivo:
+	if(nb >= 0 && nb <= 2147483647)
+	{
+		res = nb % 10;
+		c = res + '0';
+
+		if (nb <= 9)
+		{
+			write(1, &c, 1);
+			return;
+		}
+		else
+		{
+			ft_putnbr(nb / 10);
+			write(1, &c, 1);
+		}
+	}
+}
---
+++ C_00/ex07/main.c
@@ -0,0 +1,8 @@
+#include<unistd.h>
+
+void ft_putnbr(int nb);
+
+int main(void)
+{
+    ft_putnbr(42);
+}
